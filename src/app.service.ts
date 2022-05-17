06afe13a1696f13669c24800192c2059fc3f0343
Elizabeth186 <eliizabethsuarezz@gmail.com>
2022-05-16 21:40:57 -0500

    ca

--- src/app.service.ts
+++ src/app.service.ts
@@ -35,7 +35,7 @@ export class AppService {
         id: 4,
         salute: 'Fourth Element',
         properties: {
-          name: 'Item 4',
+          name: 'Items 4',
           lastname: 'Item 4',
           country: 'Item 4'
         }
